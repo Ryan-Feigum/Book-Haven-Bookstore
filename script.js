467ead415a564de235163f70569491bf91823153
Ryan Feigum <r.p.feigum@gmail.com>
2024-10-10 13:12:34 -0500

    Clean up JS file. Submitting for Task 3.1

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 
 // Newsletter
-// Get the subscribe button element
+// Add an event listener to the subscribe button
 document.getElementById("subscribe-button").addEventListener("click", () => {
   // Display the alert window
   alert("Thank you for subscribing.");
 });
 
+// About Page
+// Add an event listener to the submit button
+document.getElementById("submit-button").addEventListener("click", () => {
+    // Display the alert window
+    try {
+    alert("Thank you for your message."); } catch (error) {
+        console.log("Error: " + error);
+    }
+  });
+
+// Gallery Page
 // Initialize an empty cart array
 let cart = [];
 
-// Gallery Page
-
+// Add an event listener to the view cart button
 document.getElementById("view-cart-button").addEventListener("click", () => {
     if (cart.length > 0) {
       // Display the alert window
@@ -20,29 +30,16 @@ document.getElementById("view-cart-button").addEventListener("click", () => {
       alert("No items in cart.");
     }
   });
-/*
-// Get the add to cart buttons
-let addButton = document.getElementByClassName("add-to-cart-button")
-addButton.addEventListener("click", () => {
-    // Get the product ID
-    //let productId = document.getElementsByClassName("add-to-cart-button");
-    // Add the item to the cart
-    console.log(addButton.value);
-    try {
-        cart.push(addButton.value);
-    } catch (error) {
-        console.error(error);
-    }
-    
-    // Display the alert window
-    alert("Item added to the cart");
-    console.log(cart);
-  }); */
 
+
+  // Add an event listener to the add to cart buttons
   document.addEventListener("click", function(event) {
     if (event.target.classList.contains("add-to-cart-button")) {
+        // Get the specific product value
         const value = event.target.getAttribute("value");
+        // Add the item to the cart
         cart.push(value);
+        // Display the alert window
         alert("Item added to the cart");
         //console.log(cart);
     }
@@ -65,20 +62,10 @@ document.getElementById("clear-cart-button").addEventListener("click", () => {
 // Add an event listener to the process order button
 document.getElementById("process-order-button").addEventListener("click", () => {
   if (cart.length > 0) {
-    // Process the order
-    // (You can add your order processing logic here)
     // Display the alert window
     alert("Thank you for your order");
   } else {
     // Display the alert window
     alert("Cart is empty.");
   }
-});
-
-
-// About Page
-// Add an event listener to the submit button
-document.getElementById("submit-button").addEventListener("click", () => {
-  // Display the alert window
-  alert("Thank you for your message");
 });
